522f57e3afc696c60e20ec75a5db839049c486d4
Loxi <dianyuyi@gmail.com>
2021-11-30 21:53:12 +0800

    Update social link

--- src/data/socialMedia.js
+++ src/data/socialMedia.js
@@ -3,34 +3,33 @@ import {
   FaLinkedin,
   FaGithubSquare,
   FaMedium,
-  FaCodepen,
-} from "react-icons/fa";
+  FaTwitterSquare,
+} from 'react-icons/fa';
 
 const socialMedia = [
   {
     id: 0,
-    name: "linkedin",
+    name: 'linkedin',
     icon: <FaLinkedin />,
-    url: "https://www.linkedin.com/in/weiting-lo-a8b74b107/",
+    url: 'https://www.linkedin.com/in/weiting-lo-a8b74b107/',
   },
   {
     id: 1,
-    name: "github",
+    name: 'github',
     icon: <FaGithubSquare />,
-    url: "https://github.com/dianyuyi",
+    url: 'https://github.com/dianyuyi',
   },
   {
     id: 2,
-    name: "medium",
+    name: 'medium',
     icon: <FaMedium />,
-    url:
-      "https://medium.com/%E6%9B%B8%E5%A2%A8%E8%88%87%E7%A8%8B%E5%BC%8F%E4%B9%8B%E6%B5%B7",
+    url: 'https://medium.com/%E6%9B%B8%E5%A2%A8%E8%88%87%E7%A8%8B%E5%BC%8F%E4%B9%8B%E6%B5%B7',
   },
   {
     id: 3,
-    name: "codepen",
-    icon: <FaCodepen />,
-    url: "https://codepen.io/loxi",
+    name: 'twitter',
+    icon: <FaTwitterSquare />,
+    url: 'https://twitter.com/dianyuyi',
   },
 ];
 
